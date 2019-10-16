135383adb6e977368c4966329c2fada467cade12
Max Proske <max@mproske.com>
2019-10-15 21:30:10 -0700

    Finish lesson on strings

--- 1a_strings_and_images/strings.rkt
+++ 1a_strings_and_images/strings.rkt
@@ -1,1 +1,12 @@
-(string-append "Max" " " "Proske")
+"Apple" ; →  "Apple"
+
+; string-append append takes n arguments
+(string-append "Steve" " " "Jobs") ; → "Steve Jobs"
+
+;(+ 1 "123") ; expects a number as 2nd argument, given "123"
+
+; string-length counts characters in string
+(string-length "Apple") ; → 5
+
+; substring uses 0-based indexing
+(substring "Caribou" 2 4) ; → "ri"
